f1157f82a850b6bda06b6d24077fbc9c0dae1898
vlad28x <vlad.skibin@mail.ru>
2022-02-27 22:11:18 +0300

    Add insert-some-data migration

--- src/main/resources/db/changelog/changelog.sql
+++ src/main/resources/db/changelog/changelog.sql
@@ -27,3 +27,20 @@ create table task
         on delete cascade
         on update cascade
 );
+
+--changeset vlad28x:000001-insert-some-data
+insert into project(name, status, priority, start_date, completion_date, created_at, updated_at)
+values ('Project Management System', 'ACTIVE', 1, '2022-01-01', '2022-09-15', (select current_timestamp), (select current_timestamp)),
+       ('Car Wash Management System', 'ACTIVE', 2, '2022-02-01', '2022-06-17', (select current_timestamp), (select current_timestamp)),
+       ('Task Tracker', 'NOT_STARTED', 3, '2022-02-13', '2022-07-01', (select current_timestamp), (select current_timestamp));
+insert into task(name, description, status, priority, created_at, updated_at, project_id)
+values ('TASK-1: Create database', 'Create database in PostgreSQL DBMS', 'DONE', 1, (select current_timestamp), (select current_timestamp), 1),
+       ('TASK-2: Application Architecture', 'Create Application Architecture', 'IN_PROGRESS', 2, (select current_timestamp), (select current_timestamp), 1),
+       ('TASK-3: Entities', 'Create entities', 'TO_DO', 3, (select current_timestamp), (select current_timestamp), 1),
+       ('TASK-4: Migrations', 'Add migrations', 'TO_DO', 3, (select current_timestamp), (select current_timestamp), 1),
+       ('TASK-1: Create database', 'Create database in ЬнЫЙД DBMS', 'IN_PROGRESS', 1, (select current_timestamp), (select current_timestamp), 2),
+       ('TASK-2: Application Architecture', 'Create Application Architecture', 'IN_PROGRESS', 2, (select current_timestamp), (select current_timestamp), 2),
+       ('TASK-3: Fix a bug', 'Fix a bug in the controller', 'TO_DO', 1, (select current_timestamp), (select current_timestamp), 2),
+       ('TASK-1: Create database', 'Create database in PostgreSQL DBMS', 'DONE', 1, (select current_timestamp), (select current_timestamp), null),
+       ('TASK-2: Application Architecture', 'Create Application Architecture', 'IN_PROGRESS', 2, (select current_timestamp), (select current_timestamp), null),
+       ('TASK-3: Entities', 'Create entities', 'TO_DO', 3, (select current_timestamp), (select current_timestamp), null);
